dfbc7a0c80f444c97e7870c1f0e4f4191085e475
Sulthon Zainul Habib <sulthon.poetra.ahmad@gmail.com>
2017-08-28 16:04:57 +0700

    Update install.sh
    Update new

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-sudo apt-get install x11vnc
+sudo apt update
+sudo apt install xserver-xorg-video-dummy x11vnc
 sudo x11vnc -storepasswd "$1" /etc/x11vnc.pass
 sudo chmod a+r /etc/x11vnc.pass
 sudo cp ./xorg.conf /usr/share/X11/xorg.conf.d/xorg.conf
-chmod +x $HOME/start-vnc.sh
+cp ./start-vnc.sh ~/start-vnc.sh
+chmod +x ~/start-vnc.sh
 touch ~/x11vnc.pid
 echo  "$HOME/start-vnc.sh start" >> ~/.profile
